471b0895d0632d313cb2c591a32e2296478ef568
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2020-10-23 01:17:44 +0900

    환율 호출시마다 로더 추가

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -27,7 +27,7 @@ class _HomePageState extends State<HomePage> {
   String _receiveAmount = '';
 
   TextEditingController _transferAmountTextEditingController;
-
+  bool _isLoading = true;
   @override
   void initState() {
     _transfer = Country(name: '미국', symbol: 'USD');
@@ -42,157 +42,165 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          child: Column(
-            children: [
-              // 제목
-              Container(
-                margin: const EdgeInsets.only(top: 32, bottom: 16),
-                alignment: Alignment.center,
-                child: Text('환율 계산', style: TextStyle(fontSize: 32)),
-              ),
-              // 테이블
-              Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Table(
-                  columnWidths: {
-                    0: FlexColumnWidth(1),
-                    1: FlexColumnWidth(4),
-                  },
-                  children: [
-                    TableRow(children: [
-                      Text(
-                        '송금 국가 : ',
-                        textAlign: TextAlign.right,
-                      ),
-                      Text(_transfer.nameWithSymbol),
-                    ]),
-                    TableRow(children: [
-                      Text(
-                        '수취 국가 : ',
-                        textAlign: TextAlign.right,
-                      ),
-                      GestureDetector(
-                        child: Text(_receiver.nameWithSymbol),
-                        onTap: () {
-                          showDialog(
-                            context: context,
-                            child: SimpleDialog(
-                              title: Text('대상 국가를 선택하세요'),
-                              children: _supportedCountryList
-                                  .map((country) => SimpleDialogOption(
-                                        child: Text(country.nameWithSymbol),
-                                        onPressed: () {
-                                          setState(() => _receiver = country);
-                                          Navigator.of(context).pop();
-                                          _convertCurrency();
-                                        },
-                                      ))
-                                  .toList(),
-                            ),
-                          );
-                        },
-                      ),
-                    ]),
-                    TableRow(children: [
-                      Text(
-                        '환율 : ',
-                        textAlign: TextAlign.right,
-                      ),
-                      Text(
-                          '${_exchange?.receiver?.format?.format(_exchange?.rate) ?? ""}'),
-                    ]),
-                    TableRow(children: [
-                      Text(
-                        '조회 시간 : ',
-                        textAlign: TextAlign.right,
-                      ),
-                      Text(_requestDateTime),
-                    ]),
-                    TableRow(
-                      decoration: BoxDecoration(),
+        body: Stack(
+          children: [
+            if (_isLoading) LinearProgressIndicator(),
+            Container(
+              child: Column(
+                children: [
+                  // 제목
+                  Container(
+                    margin: const EdgeInsets.only(top: 32, bottom: 16),
+                    alignment: Alignment.center,
+                    child: Text('환율 계산', style: TextStyle(fontSize: 32)),
+                  ),
+                  // 테이블
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Table(
+                      columnWidths: {
+                        0: FlexColumnWidth(1),
+                        1: FlexColumnWidth(4),
+                      },
                       children: [
-                        Text(
-                          '송금액 : ',
-                          textAlign: TextAlign.right,
-                        ),
-                        Flex(
-                          direction: Axis.horizontal,
+                        TableRow(children: [
+                          Text(
+                            '송금 국가 : ',
+                            textAlign: TextAlign.right,
+                          ),
+                          Text(_transfer.nameWithSymbol),
+                        ]),
+                        TableRow(children: [
+                          Text(
+                            '수취 국가 : ',
+                            textAlign: TextAlign.right,
+                          ),
+                          GestureDetector(
+                            child: Text(_receiver.nameWithSymbol),
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                child: SimpleDialog(
+                                  title: Text('대상 국가를 선택하세요'),
+                                  children: _supportedCountryList
+                                      .map((country) => SimpleDialogOption(
+                                            child: Text(country.nameWithSymbol),
+                                            onPressed: () {
+                                              setState(
+                                                  () => _receiver = country);
+                                              Navigator.of(context).pop();
+                                              _convertCurrency();
+                                            },
+                                          ))
+                                      .toList(),
+                                ),
+                              );
+                            },
+                          ),
+                        ]),
+                        TableRow(children: [
+                          Text(
+                            '환율 : ',
+                            textAlign: TextAlign.right,
+                          ),
+                          Text(
+                              '${_exchange?.receiver?.format?.format(_exchange?.rate) ?? ""}'),
+                        ]),
+                        TableRow(children: [
+                          Text(
+                            '조회 시간 : ',
+                            textAlign: TextAlign.right,
+                          ),
+                          Text(_requestDateTime),
+                        ]),
+                        TableRow(
+                          decoration: BoxDecoration(),
                           children: [
-                            Expanded(
-                              flex: 2,
-                              child: Container(
-                                child: Form(
-                                  key: _formKey,
-                                  child: TextFormField(
-                                    controller:
-                                        _transferAmountTextEditingController,
-                                    onFieldSubmitted: _onFieldSubmitted,
-                                    validator: (value) {
-                                      try {
-                                        double amount = double.parse(value);
-                                        if (amount < 0 || amount > 10000) {
-                                          throw Error();
-                                        }
-                                        return null;
-                                      } catch (e) {
-                                        return '송금액이 바르지 않습니다.';
-                                      }
-                                    },
-                                    onSaved: (String value) {
-                                      setState(() => _transferAmount =
-                                          double.parse(value));
-                                    },
-                                    keyboardType:
-                                        TextInputType.numberWithOptions(
-                                            decimal: false, signed: false),
-                                    textAlign: TextAlign.right,
-                                    decoration: InputDecoration(
-                                      contentPadding:
-                                          const EdgeInsets.symmetric(
-                                        horizontal: 4,
-                                        vertical: 0,
-                                      ),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(0),
-                                        borderSide: BorderSide(
-                                            color: Colors.blueAccent,
-                                            width: 1.0),
-                                      ),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(0),
-                                        borderSide: BorderSide(
-                                            color: Colors.grey, width: 1.0),
+                            Text(
+                              '송금액 : ',
+                              textAlign: TextAlign.right,
+                            ),
+                            Flex(
+                              direction: Axis.horizontal,
+                              children: [
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    child: Form(
+                                      key: _formKey,
+                                      child: TextFormField(
+                                        controller:
+                                            _transferAmountTextEditingController,
+                                        onFieldSubmitted: _onFieldSubmitted,
+                                        validator: (value) {
+                                          try {
+                                            double amount = double.parse(value);
+                                            if (amount < 0 || amount > 10000) {
+                                              throw Error();
+                                            }
+                                            return null;
+                                          } catch (e) {
+                                            return '송금액이 바르지 않습니다.';
+                                          }
+                                        },
+                                        onSaved: (String value) {
+                                          setState(() => _transferAmount =
+                                              double.parse(value));
+                                        },
+                                        keyboardType:
+                                            TextInputType.numberWithOptions(
+                                                decimal: false, signed: false),
+                                        textAlign: TextAlign.right,
+                                        decoration: InputDecoration(
+                                          contentPadding:
+                                              const EdgeInsets.symmetric(
+                                            horizontal: 4,
+                                            vertical: 0,
+                                          ),
+                                          focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(0),
+                                            borderSide: BorderSide(
+                                                color: Colors.blueAccent,
+                                                width: 1.0),
+                                          ),
+                                          enabledBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(0),
+                                            borderSide: BorderSide(
+                                                color: Colors.grey, width: 1.0),
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
+                                Expanded(
+                                    child: Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text(_transfer.symbol),
+                                ))
+                              ],
                             ),
-                            Expanded(
-                                child: Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: Text(_transfer.symbol),
-                            ))
                           ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-              // 결과
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Text(
-                    '수취금액은 $_receiveAmount 입니다.',
-                    style: TextStyle(fontSize: 16),
                   ),
-                ),
-              )
-            ],
-          ),
+                  // 결과
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        '수취금액은 $_receiveAmount 입니다.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -210,6 +218,9 @@ class _HomePageState extends State<HomePage> {
 
   // 수취 국가가 변경되거나, 송금액이 변경되면 호출하여 환전 결과를 반영합니다.
   void _convertCurrency() {
+    setState(() {
+      _isLoading = true;
+    });
     _requestAPI(Exchange(
       transfer: _transfer,
       receiver: _receiver,
@@ -220,6 +231,8 @@ class _HomePageState extends State<HomePage> {
         _requestDateTime = _formatDateTime(exchange.timestamp.toLocal());
         _receiveAmount = _receiver.format.format(_exchange.receiveAmount);
       });
+    }).whenComplete(() {
+      setState(() => _isLoading = false);
     });
   }
 
